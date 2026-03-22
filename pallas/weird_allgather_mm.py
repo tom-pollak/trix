520fb087187e401a32ca6b7efa34376e255763b1
Tom Pollak <tompollak1000@gmail.com>
2026-03-22 10:30:14 +0000

    fixing all gatrher

--- pallas/weird_allgather_mm.py
+++ pallas/weird_allgather_mm.py
@@ -19,6 +19,7 @@ import os
 os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=8"
 
 from functools import partial
+import numpy as np
 import jax
 import jax.numpy as jnp
 import jax.experimental.pallas as pl
@@ -31,16 +32,13 @@ if INTERPRET:
 mesh = jax.make_mesh((8,), ("x",), (jax.sharding.AxisType.Explicit,))
 jax.set_mesh(mesh)
 
-# %%
-
-
 
 # %%
 
 
-def matmul(x_ref, y_ref, z_ref, block_shape):
+def matmul(x_ref, y_ref, z_ref, acc_vmem, block_shape, write_z: bool, zero_acc: bool):
     def matmul_kernel(x_vmem, y_vmem, z_vmem, acc_vmem):
-        @pl.when(pl.program_id(2) == 2)
+        @pl.when(zero_acc & (pl.program_id(2) == 0))
         def _():
             acc_vmem[...] = jnp.zeros_like(acc_vmem)
 
@@ -49,7 +47,7 @@ def matmul(x_ref, y_ref, z_ref, block_shape):
 
         acc_vmem[...] += jnp.dot(x, y, preferred_element_type=jnp.float32)
 
-        @pl.when(pl.program_id(2) == pl.num_programs(2) - 1)
+        @pl.when(write_z & (pl.program_id(2) == pl.num_programs(2) - 1))
         def _():
             z_vmem[...] = acc_vmem[...].astype(z_vmem.dtype)
 
@@ -62,25 +60,23 @@ def matmul(x_ref, y_ref, z_ref, block_shape):
     y_spec = pl.BlockSpec((bk, bn), lambda i, j, k: (k, j))
     z_spec = pl.BlockSpec((bm, bn), lambda i, j, k: (i, j))
 
-    @partial(pl.core_map, acc_vmem=pltpu.VMEM((bm, bn), dtype=jnp.float32))
-    def _(acc_vmem):
-        pltpu.emit_pipeline(
-            partial(matmul, acc_vmem=acc_vmem),
-            grid=grid,
-            in_specs=[x_spec, y_spec],
-            out_specs=[z_spec],
-            should_accumulate_out=True,
-            dimension_semantics=("parallel", "parallel", "arbitary"),
-        )(x_ref, y_ref, z_ref)
-
-    return z_ref
+    # pltpu.VMEM((bm, bn), dtype=jnp.float32)
+    pltpu.emit_pipeline(
+        matmul,
+        grid=grid,
+        in_specs=[x_spec, y_spec],
+        out_specs=[z_spec],
+        should_accumulate_out=True,
+        dimension_semantics=(pltpu.PARALLEL, pltpu.PARALLEL, pltpu.ARBITRARY),
+    )(x_ref, y_ref, z_ref)
 
 
-@jax.jit
+@jax.jit(static_argnames=["block_shape"])
 @jax.shard_map(mesh=mesh, out_specs=jax.P(None, "x"))
-def all_gather_matmul(x_s, y, block_shape):
+def all_gather_matmul(x_s, y, block_shape, check_vma=False):
     m, k_s = x_s.shape
     k, n = y.shape
+    bm, bk, bn = block_shape
     z = jnp.empty((m, n), dtype=x_s.dtype)
 
     x_hbm = jax.new_ref(x_s)
@@ -91,29 +87,39 @@ def all_gather_matmul(x_s, y, block_shape):
     xp_left_hbm = jax.new_ref(jnp.empty_like(x_s))
     xp_right_hbm = jax.new_ref(jnp.empty_like(x_s))
 
-    @pl.core_map(pltpu.create_tensorcore_mesh("core"), interpret=INTERPRET)
-    def _(acc_vmem):
-        device_idx, axis_size = jax.lax.axis_index("x"), jax.lax.axis_size("x")
+    device_idx, axis_size = jax.lax.axis_index("x"), jax.lax.axis_size("x")
+
+    @pl.kernel(
+        mesh=pltpu.create_tensorcore_mesh("core", num_cores=2),
+        scratch_shapes=dict(
+            acc_vmem=pltpu.VMEM((bm, bn), dtype=jnp.float32),
+            local_sems=pltpu.SemaphoreType.DMA((2,)),
+        ),
+        interpret=INTERPRET,
+    )
+    def _(acc_vmem, local_sems):
         left_idx = (device_idx - 1) % axis_size
         right_idx = (device_idx + 1) % axis_size
 
-        matmul(x_hbm, y_hbm, z_hbm, block_shape)
+        fut_left = pltpu.async_copy(x_hbm, xp_left_hbm, local_sems.at[0])
+        fut_right = pltpu.async_copy(x_hbm, xp_right_hbm, local_sems.at[1])
+
+        matmul(x_hbm, y_hbm, z_hbm, acc_vmem, block_shape, write_z=False, zero_acc=True)
 
         # Sync all devices
         sem_barrier = pltpu.get_barrier_semaphore()
+        pltpu.semaphore_signal(sem_barrier, 1, device_id={"x": left_idx})
         pltpu.semaphore_signal(sem_barrier, 1, device_id={"x": right_idx})
+        pltpu.semaphore_wait(sem_barrier, 2)  # Ensure sync
 
-        # pltpu.sync_copy(x_hbm, xp_hbm)
-
-        pltpu.semaphore_wait(sem_barrier, 1)  # Ensure sync
-
-        orig = (x_hbm.at[:, pl.ds(device_idx * k_s, k_s)],)
+        fut_left.wait()
+        fut_right.wait()
 
         @partial(
             pl.run_scoped,
             send_sems=pltpu.SemaphoreType.DMA((axis_size - 1,)),
             recv_sems=pltpu.SemaphoreType.DMA((axis_size - 1,)),
-            collective_axis="x",
+            collective_axes="x",
         )
         def gather(send_sems, recv_sems):
             def bi_send(i):
@@ -134,20 +140,41 @@ def all_gather_matmul(x_s, y, block_shape):
                 return fut_left, fut_right
 
             for i in range(0, (axis_size - 1), 2):
+                write_z = i == axis_size - 2
                 fut_left, fut_right = bi_send(i)
 
-                # these can be done in parallel.
+                # TODO: done in parallel?
                 fut_left.wait_recv()
-                matmul(xp_left_hbm, y_hbm, z_hbm, block_shape)
+                matmul(
+                    xp_left_hbm,
+                    y_hbm,
+                    z_hbm,
+                    acc_vmem,
+                    block_shape,
+                    write_z=write_z,
+                    zero_acc=False,
+                )
 
                 fut_right.wait_recv()
-                matmul(xp_right_hbm, y_hbm, z_hbm, block_shape)
+                matmul(
+                    xp_right_hbm,
+                    y_hbm,
+                    z_hbm,
+                    acc_vmem,
+                    block_shape,
+                    write_z=write_z,
+                    zero_acc=False,
+                )
 
-                # do we need a full wait here?
+                fut_left.wait_send()
+                fut_right.wait_send()
 
 
-# x = np.arange(512).reshape(256, 2)
-# x = jax.device_put(x, jax.P(None, "device"))
-# all_gather(x)
+block_shape = (16, 16, 16)
+x = np.arange(2048).reshape(32, 64)
+y = np.arange(2048).reshape(32, 64)
+x = jax.device_put(x, jax.P(None, "x"))
+y = jax.device_put(y, jax.P(None, "x"))
+all_gather_matmul(x, y, block_shape)
 
 # %%
