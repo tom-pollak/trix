31317bd323a4a2f4f82611ed93e02e57ba487e57
Tom Pollak <tompollak1000@gmail.com>
2026-03-26 14:27:51 +0000

    innfer shape

--- pallas/weird_allgather_mm.py
+++ pallas/weird_allgather_mm.py
@@ -14,9 +14,13 @@ I'm also intentionally not using pl.pallas_call / pl.kernel
 """
 
 # %%
-import os
+# import os
 
-os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=8"
+# os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=8"
+
+import lovely_jax as lt
+
+lt.monkey_patch()
 
 from functools import partial
 import jax
@@ -27,7 +31,7 @@ import jax.experimental.pallas.tpu as pltpu
 if jax.default_backend() == "cpu":
     pltpu.set_tpu_interpret_mode(pltpu.InterpretParams(num_cores_or_threads=2))
 
-mesh = jax.make_mesh((8,), ("x",), (jax.sharding.AxisType.Explicit,))
+mesh = jax.make_mesh((jax.device_count(),), ("x",), (jax.sharding.AxisType.Explicit,))
 jax.set_mesh(mesh)
 
 
@@ -72,32 +76,33 @@ BLOCK_SHAPE = (256, 256, 256)
 
 
 @jax.jit
-@jax.shard_map(mesh=mesh, out_specs=jax.P(None, "x"), check_vma=False)
+@jax.shard_map(mesh=mesh, out_specs=jax.P(None, None), check_vma=False)
 def all_gather_matmul(x_s, y):
     bm, bk, bn = BLOCK_SHAPE
     m, k_s = x_s.shape
     k, n = y.shape
-    z = jnp.empty((m, n), dtype=x_s.dtype)
 
     x_hbm = jax.new_ref(x_s)
     y_hbm = jax.new_ref(y)
 
     # blocks of x
-    xp_left_hbm = jax.new_ref(jnp.empty_like(x_s))
-    xp_right_hbm = jax.new_ref(jnp.empty_like(x_s))
+    xp_left_hbm = jax.new_ref(jnp.empty(x_s.shape, dtype=x_s.dtype))
+    xp_right_hbm = jax.new_ref(jnp.empty_like(xp_left_hbm))
 
     device_idx, axis_size = jax.lax.axis_index("x"), jax.lax.axis_size("x")
 
     @pl.kernel(
         mesh=pltpu.create_tensorcore_mesh("core", num_cores=2),
-        out_shape=jax.ShapeDtypeStruct(z.shape, z.dtype),
+        out_shape=jax.ShapeDtypeStruct((m, n), x.dtype),
         scratch_shapes=dict(
             acc_vmem=pltpu.VMEM((bm, bn), dtype=jnp.float32),
             local_sems=pltpu.SemaphoreType.DMA((2,)),
+            send_sems=pltpu.SemaphoreType.DMA((axis_size - 1,)),
+            recv_sems=pltpu.SemaphoreType.DMA((axis_size - 1,)),
         ),
         compiler_params=pltpu.CompilerParams(collective_id=0),
     )
-    def kernel(z_hbm, acc_vmem, local_sems):
+    def kernel(z_hbm, acc_vmem, local_sems, send_sems, recv_sems):
         left_idx = (device_idx - 1) % axis_size
         right_idx = (device_idx + 1) % axis_size
 
@@ -118,57 +123,51 @@ def all_gather_matmul(x_s, y):
         fut_left.wait()
         fut_right.wait()
 
-        @partial(
-            pl.run_scoped,
-            send_sems=pltpu.SemaphoreType.DMA((axis_size - 1,)),
-            recv_sems=pltpu.SemaphoreType.DMA((axis_size - 1,)),
-        )
-        def gather(send_sems, recv_sems):
-            def bi_send(i):
-                fut_left = pltpu.async_remote_copy(
-                    xp_left_hbm,
-                    xp_left_hbm,
-                    send_sem=send_sems.at[i],
-                    recv_sem=recv_sems.at[i],
-                    device_id={"x": left_idx},
-                )
-                fut_right = pltpu.async_remote_copy(
-                    xp_right_hbm,
-                    xp_right_hbm,
-                    send_sem=send_sems.at[i + 1],
-                    recv_sem=recv_sems.at[i + 1],
-                    device_id={"x": right_idx},
-                )
-                return fut_left, fut_right
+        def bi_send(i):
+            fut_left = pltpu.async_remote_copy(
+                xp_left_hbm,
+                xp_left_hbm,
+                send_sem=send_sems.at[i],
+                recv_sem=recv_sems.at[i],
+                device_id={"x": left_idx},
+            )
+            fut_right = pltpu.async_remote_copy(
+                xp_right_hbm,
+                xp_right_hbm,
+                send_sem=send_sems.at[i + 1],
+                recv_sem=recv_sems.at[i + 1],
+                device_id={"x": right_idx},
+            )
+            return fut_left, fut_right
 
-            for i in range(0, (axis_size - 1), 2):
-                write_z = i == axis_size - 2
-                fut_left, fut_right = bi_send(i)
+        for i in range(0, (axis_size - 1), 2):
+            write_z = i == axis_size - 2
+            fut_left, fut_right = bi_send(i)
 
-                fut_left.wait_recv()
-                matmul(
-                    xp_left_hbm,
-                    y_hbm,
-                    z_hbm,
-                    acc_vmem,
-                    BLOCK_SHAPE,
-                    write_z=write_z,
-                    zero_acc=False,
-                )
+            fut_left.wait_recv()
+            matmul(
+                xp_left_hbm,
+                y_hbm,
+                z_hbm,
+                acc_vmem,
+                BLOCK_SHAPE,
+                write_z=write_z,
+                zero_acc=False,
+            )
 
-                fut_right.wait_recv()
-                matmul(
-                    xp_right_hbm,
-                    y_hbm,
-                    z_hbm,
-                    acc_vmem,
-                    BLOCK_SHAPE,
-                    write_z=write_z,
-                    zero_acc=False,
-                )
+            fut_right.wait_recv()
+            matmul(
+                xp_right_hbm,
+                y_hbm,
+                z_hbm,
+                acc_vmem,
+                BLOCK_SHAPE,
+                write_z=write_z,
+                zero_acc=False,
+            )
 
-                fut_left.wait_send()
-                fut_right.wait_send()
+            fut_left.wait_send()
+            fut_right.wait_send()
 
     return kernel()
 
@@ -177,10 +176,12 @@ x = jax.random.uniform(jax.random.key(0), (2048, 2048), dtype=jnp.bfloat16)
 y = jax.random.uniform(jax.random.key(1), (2048, 2048), dtype=jnp.bfloat16)
 
 x = jax.device_put(x, jax.P(None, "x"))
-y = jax.device_put(y, jax.P(None, "x"))
+y = jax.device_put(y, jax.P(None, None))
 
 z = all_gather_matmul(x, y)
 z_ref = x @ y
+print(z.v)
+print(z_ref.v)
 assert jnp.allclose(z, z_ref)
 
 # %%
